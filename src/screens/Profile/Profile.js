52f565faa71570107d092499339ba909ff6f7cd9
younusraza909 <younusraza909@gmail.com>
2023-04-14 23:28:13 +0500

    basic Layout and getting updaing profile using gallery image

--- src/screens/Profile/Profile.js
+++ src/screens/Profile/Profile.js
@@ -1,14 +1,147 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { View, Text, TextInput, TouchableOpacity, Image, StyleSheet, Alert } from 'react-native';
+import { Camera } from 'expo-camera';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as ImagePicker from 'expo-image-picker';
 
+const Profile = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [contact, setContact] = useState('');
+    const [image, setImage] = useState(null);
+    const [hasCameraPermission, setHasCameraPermission] = useState(null);
+    const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
+    const cameraRef = useRef(null)
 
-const ProfileScreen = () => {
+    useEffect(() => {
+        (async () => {
+            const cameraStatus = await Camera.requestCameraPermissionsAsync();
+            setHasCameraPermission(cameraStatus.status === 'granted');
+            const galleryStatus = await ImagePicker.requestMediaLibraryPermissionsAsync();
+            setHasGalleryPermission(galleryStatus.status === 'granted');
+
+            const profileData = await AsyncStorage.getItem('profile');
+            if (profileData) {
+                const { name, email, contact, image } = JSON.parse(profileData);
+                setName(name);
+                setEmail(email);
+                setContact(contact);
+                setImage(image);
+            }
+        })();
+    }, []);
+
+    const saveProfileData = async () => {
+        const data = { name, email, contact, image };
+        await AsyncStorage.setItem('profile', JSON.stringify(data));
+        Alert.alert('Profile updated')
+    };
+
+    const takePicture = async () => {
+        if (cameraRef.current) {
+            const photo = await cameraRef.current.takePictureAsync();
+            setImage(photo.uri);
+        }
+    };
+
+    const pickImageFromGallery = async () => {
+        const result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [1, 1],
+            quality: 1,
+        });
+
+        if (!result.cancelled) {
+            setImage(result.uri);
+        }
+    };
 
     return (
-        <View>
-            <Text>Profile</Text>
+        <View style={styles.container}>
+            <TouchableOpacity style={styles.imageContainer} onPress={pickImageFromGallery}>
+                {image ? (
+                    <Image source={{ uri: image }} style={styles.image} />
+                ) : (
+                    <View style={styles.imagePlaceholder}>
+                        <Text style={styles.imagePlaceholderText}>Add Photo</Text>
+                    </View>
+                )}
+            </TouchableOpacity>
+            <TextInput w style={styles.input} placeholder="Name" value={name} onChangeText={setName} />
+            <TextInput style={styles.input} placeholder="Email" value={email} onChangeText={setEmail} />
+            <TextInput
+                style={styles.input}
+                placeholder="Contact Number"
+                value={contact}
+                onChangeText={setContact}
+                keyboardType="phone-pad"
+            />
+
+            <TouchableOpacity style={styles.button} onPress={saveProfileData}>
+                <Text style={styles.buttonText}>Update Profile</Text>
+            </TouchableOpacity>
+
+
+            <TouchableOpacity style={styles.cameraContainer} onPress={takePicture}>
+                <Camera ref={cameraRef} style={styles.camera} type={Camera.Constants.Type.back} />
+            </TouchableOpacity>
+
         </View>
     );
 };
 
-export default ProfileScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        backgroundColor: '#fff',
+        paddingVertical: 30,
+        paddingHorizontal: 20,
+    },
+    imageContainer: {
+        width: 150,
+        height: 150,
+        borderRadius: 75,
+        overflow: 'hidden',
+        marginBottom: 30,
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+    },
+    inputContainer: {
+        width: '100%',
+        marginBottom: 20,
+    },
+    inputLabel: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 5,
+        width: "100%",
+        padding: 10,
+        fontSize: 16,
+        marginBottom: 10,
+    },
+    button: {
+        backgroundColor: '#2196F3',
+        borderRadius: 5,
+        padding: 10,
+        width: '100%',
+        alignItems: 'center',
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+});
+
+export default Profile;
